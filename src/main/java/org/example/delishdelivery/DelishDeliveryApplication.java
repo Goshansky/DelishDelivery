d67f5f1b2d174d82239c918d1031d26f6c84bd63
Гошанский <mc.goshanskyi@yandex.ru>
2024-11-10 22:37:22 +0300

    init commit

---
+++ src/main/java/org/example/delishdelivery/DelishDeliveryApplication.java
@@ -0,0 +1,13 @@
+package org.example.delishdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelishDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelishDeliveryApplication.class, args);
+    }
+
+}
