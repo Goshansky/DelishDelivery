f50b6858d2763460293d792736fefdcee2fc0010
Гошанский <mc.goshanskyi@yandex.ru>
2024-11-16 22:16:37 +0300

    restaurant: исправлен базовый микросервис

---
+++ restaurant/src/main/java/org/example/restaurant/services/RestaurantService.java
@@ -0,0 +1,25 @@
+package org.example.restaurant.services;
+
+import org.example.restaurant.entities.Restaurant;
+import org.example.restaurant.repositories.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RestaurantService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantRepository.findAll();
+    }
+
+    public Optional<Restaurant> getRestaurantById(Long id) {
+        return restaurantRepository.findById(id);
+    }
+
+}
